bc288db67bc67fc3bd967e3ac605975c76792f27
Dizy <dev@dizy.cc>
2021-06-07 15:17:43 +0800

    Update deploy workflow

---
+++ scripts/deploy_set_env.php
@@ -0,0 +1,28 @@
+<?php
+
+function setEnv(string $key, string $value) {
+  $gitHubEnv = $_SERVER['GITHUB_ENV'];
+  file_put_contents($gitHubEnv, "$key=$value\n", FILE_APPEND);
+}
+
+$deployPath = trim($_SERVER['DEPLOY_PATH'], '\\/');
+
+$deployList = [
+  'packages/',
+  'vender/',
+  '.htaccess',
+  'composer.json',
+  'extend.php',
+  'flarum',
+  'index.php',
+  'site.php'
+];
+
+$packageListStr = join(' ', $deployList);
+setEnv('PACKAGE_LIST', $packageListStr);
+
+$removeList = array_map(function($item) use ($deployPath){
+  return "$deployPath/$item";
+}, $deployList);
+$removeListStr = join(' ', $removeList);
+setEnv('REMOVE_LIST', $removeListStr);
